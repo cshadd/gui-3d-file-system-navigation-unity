97c9973405a8d9b76664ea1e3d2b330d0fad3194
cshadd <christian.v.t.shadd@gmail.com>
2019-11-17 17:07:53 -0800

    Added islands for drives

--- Gui 3d File System Navigation Unity/Assets/Scripts/Manager/FileManager.cs
+++ Gui 3d File System Navigation Unity/Assets/Scripts/Manager/FileManager.cs
@@ -9,8 +9,8 @@ namespace Gui3dFileSystemNavigationUnity.Manager
     public class FileManager : MonoBehaviour
     {
         private int count;
-        [SerializeField]
-        private List<DriveNode> driveNodes;
+        //[SerializeField]
+        //private List<DriveNode> driveNodes;
         [SerializeField]
         private FileUIConnectorManager uiConnector;
         private RaycastHit hitInfo;
@@ -21,11 +21,50 @@ namespace Gui3dFileSystemNavigationUnity.Manager
 
         private class Root : DirectoryNode
         {
-            [SerializeField]
-            private List<DriveNode> driveNodes;
+            public List<DriveNode> driveNodes = new List<DriveNode>();
 
-            public Root() : base("") { return; }
-
+            public new ISystemNode<DirectoryInfo> Assign(DirectoryInfo container,
+                DirectoryNode parent = null)
+            {
+                throw new NotSupportedException("This method is not supported for a Root.");
+            }
+            public override ISystemNode<DirectoryInfo> Grab(string path)
+            {
+                throw new NotSupportedException("This method is not supported for a Root.");
+            }
+            public new ISystemNode<DirectoryInfo> Populate()
+            {
+                var sample = new GameObject();
+                Populate(sample, sample);
+                Destroy(sample);
+                return this;
+            }
+            public ISystemNode<DirectoryInfo> Populate(PrimitiveType drivePrimitiveType)
+            {
+                var sample = GameObject.CreatePrimitive(drivePrimitiveType);
+                Populate(sample);
+                Destroy(sample);
+                return this;
+            }
+            [Obsolete("This method is obsolete.")]
+            public new ISystemNode<DirectoryInfo> Populate(PrimitiveType drivePrimitiveType,
+                PrimitiveType notUsed)
+            {
+                return Populate(drivePrimitiveType);
+            }
+            public new ISystemNode<DirectoryInfo> Populate(GameObject driveTemplate,
+                GameObject notUsed = null)
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    var driveGameObject = Instantiate(driveTemplate);
+                    driveGameObject.transform.parent = transform;
+                    var driveNode = driveGameObject.AddComponent<DriveNode>();
+                    driveNode.Assign(drive, this);
+                    driveNodes.Add(driveNode);
+                }
+                return this;
+            }
         }
 
         private Vector3 createIsland(DirectoryNode directoryNode)
@@ -65,21 +104,23 @@ namespace Gui3dFileSystemNavigationUnity.Manager
 
         private void Start()
         {
-            root = gameObject.AddComponent<Root>();
             Debug.Log(Application.productName + " started.");
+            root = gameObject.AddComponent<Root>();
+            root.Populate(PrimitiveType.Cylinder);
             count = 0;
-            var drivePosition = 0;
+
+            /*var drivePosition = 0;
             foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
-                var driveObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                driveObject.transform.parent = transform.parent;
-                driveObject.transform.position = new Vector3(0 - drivePosition, 0 , 10);
-                driveObject.transform.localScale = new Vector3(1, 1, 1);
-                var driveNode = driveObject.AddComponent<DriveNode>();
+                var driveGameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                driveGameObject.transform.parent = transform.parent;
+                driveGameObject.transform.position = new Vector3(0 - drivePosition, 0 , 10);
+                driveGameObject.transform.localScale = new Vector3(1, 1, 1);
+                var driveNode = driveGameObject.AddComponent<DriveNode>();
                 driveNode.Assign(drive);
                 driveNodes.Add(driveNode);
 
-                var renderer = driveObject.GetComponent<Renderer>();
+                var renderer = driveGameObject.GetComponent<Renderer>();
                 if (driveNode.extendedInfo.isAccessDenied)
                 {
                     renderer.material.SetColor("_Color", Color.red);
@@ -90,6 +131,39 @@ namespace Gui3dFileSystemNavigationUnity.Manager
                 }
 
                 drivePosition += 15;
+            }*/
+
+            var islandPosition = new Vector3(0, 0, 0);
+            islandPosition = createIsland(root);
+
+            count++;
+
+            var x = -4;
+            var y = 0;
+            var z = 4;
+            foreach (DriveNode childDriveNode in root.driveNodes)
+            {
+                childDriveNode.transform.position = new Vector3(
+                    islandPosition.x + x,
+                    islandPosition.y + y,
+                    islandPosition.z + z);
+
+                x += 1;
+                if (x >= 5)
+                {
+                    x = -4;
+                    z -= 1;
+                }
+
+                var renderer = childDriveNode.GetComponent<Renderer>();
+                if (childDriveNode.extendedInfo.isAccessDenied)
+                {
+                    renderer.material.SetColor("_Color", Color.red);
+                }
+                else
+                {
+                    renderer.material.SetColor("_Color", Color.gray);
+                }
             }
 
             return;
